30cc283160b75011f8b1b57881a30f610566bdd5
strohynis <strohynis@strohynisnoMacBook-Pro.local>
2022-06-30 23:35:29 +0900

    モデルの作成

---
+++ db/migrate/20220629151040_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+      t.string :email
+      t.integer :sex
+      t.date :birthday
+      t.string :avatar
+      t.text :self_introduction
+
+      t.timestamps
+    end
+  end
+end
