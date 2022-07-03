2dd6c1b4382eb575af28d0f1856ec1bd4604b187
strohynis <85390184+strohynis@users.noreply.github.com>
2022-07-04 01:58:12 +0900

    モデルファイルの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  # mount_uploader :image, ImageUploader
+  belongs_to :user
+  belongs_to :category
+
+  has_many :post_files, dependent: :destroy
+  accepts_nested_attributes_for :post_files
+
+  validates :text, presence: true, length: { maximum: 300 }
 end
