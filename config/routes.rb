3ca47dad7c6f3c52dc7b4915a94eaccfe2d595d5
strohynis <85390184+strohynis@users.noreply.github.com>
2022-07-04 02:10:38 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users, except: [:index]#アカウント操作に関するアクションを設定
+  resources :posts, except: [:index]#投稿に関するアクションを設定
 end
