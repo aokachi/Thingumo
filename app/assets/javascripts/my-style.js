7d4c28038f6ac694e7d09a9b40fe46100c686da7
strohynis <85390184+strohynis@users.noreply.github.com>
2022-07-18 03:30:02 +0900

    投稿機能の作成 投稿作成ページのドロップダウンの作成②

---
+++ app/assets/javascripts/my-style.js
@@ -0,0 +1,22 @@
+/*Dropdown Menu*/
+$('.post-dropdown').click(function () {
+  $(this).attr('tabindex', 1).focus();
+  $(this).toggleClass('active');
+  $(this).find('.post-dropdown-menu').slideToggle(300);
+});
+$('.post-dropdown').focusout(function () {
+  $(this).removeClass('active');
+  $(this).find('.post-dropdown-menu').slideUp(300);
+});
+$('.post-dropdown .post-dropdown-menu li').click(function () {
+  $(this).parents('.post-dropdown').find('span').text($(this).text());
+  $(this).parents('.post-dropdown').find('input').attr('value', $(this).attr('id'));
+});
+/*End Dropdown Menu*/
+
+
+$('.post-dropdown-menu li').click(function () {
+var input = '<strong>' + $(this).parents('.post-dropdown').find('input').val() + '</strong>',
+msg = '<span class="msg">Hidden input value: ';
+$('.msg').html(msg + input + '</span>');
+}); 
