e26848690a0d24b86f86431dce0de27973001c23
strohynis <85390184+strohynis@users.noreply.github.com>
2022-07-18 00:23:35 +0900

    投稿機能 投稿作成ページのドロップダウン導入前

--- app/models/post_file.rb
+++ app/models/post_file.rb
@@ -1,6 +1,6 @@
 class PostFile < ApplicationRecord
   belongs_to :post
 
-  #画像アップロード
+  # 画像アップロード
   mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :users, except: [:index]#アカウント操作に関するアクションを設定
-  resources :posts, except: [:index]#投稿に関するアクションを設定
+  resources :users, except: [:index]
+  resources :posts, except: [:index]
+  resources :categories, only: [:index]
 end
