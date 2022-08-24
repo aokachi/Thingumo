f977bc42c3e873298be465a87e62c7f74474aab5
strohynis <85390184+strohynis@users.noreply.github.com>
2022-08-25 00:01:22 +0900

    投稿のカテゴリ別表示機能の作成②

--- app/controllers/categories_controller.rb
+++
@@ -1,7 +0,0 @@
-class CategoriesController < ApplicationController
-
-  def index
-   @posts = Post.where(category_id: params[:category_id]).page(params[:page]).per(15).search(params[:search])
-   @categories = Category.all
-  end
-end
