97dfaf15c59e158527bedf9acfb2e19710a5d555
strohynis <85390184+strohynis@users.noreply.github.com>
2023-05-28 22:10:09 +0900

    アカウント作成ボタンの修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -8,14 +8,4 @@ class ToppagesController < ApplicationController
       @posts = Post.order(created_at: :desc).all.page(params[:page]).per(15)
     end
   end
-
-
-
-    # @categories = Category.all
-    # if params[:category_id]
-      # @posts = posts.where(category_id: params[:category_id]).order(created_at: :desc).page(params[:page]).per(15) #該当カテゴリの投稿を表示
-    # else
-      # @posts = Post.all.order(created_at: :desc).page(params[:page]).per(15) #全投稿の表示
-    # end
-  # end
 end
