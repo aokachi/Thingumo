441650be61f808651ceb63a87a107a3a48479b35
strohynis <85390184+strohynis@users.noreply.github.com>
2023-07-04 00:27:58 +0900

    コメント（回答）入力制限の作成、入力候補の作成

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,41 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
   let modalButton = document.getElementById('modal-button');
+  let modal = document.getElementById('modal');
+  let disclaimer = document.getElementById('disclaimer');
+  let specialAnswerForm = document.getElementById('modal-form');
+  let disagreeButton = document.getElementById('disagree-button');
+  let agreeButton = document.getElementById('agree-button');
+
   modalButton.addEventListener('click', function(){
-    let modal = document.getElementById('modal');
-    let disclaimer = document.getElementById('disclaimer');
-    let specialAnswerForm = document.getElementById('modal-form');
     modal.style.display = 'block';
     disclaimer.style.display = 'block';
     specialAnswerForm.style.display = 'none';
   });
 
-  let disagreeButton = document.getElementById('disagree-button');
   disagreeButton.addEventListener('click', function(){
-    let modal = document.getElementById('modal');
     modal.style.display = 'none';
   });
 
-  let agreeButton = document.getElementById('agree-button');
   agreeButton.addEventListener('click', function(){
-    let disclaimer = document.getElementById('disclaimer');
-    let specialAnswerForm = document.getElementById('modal-form');
     disclaimer.style.display = 'none';
     specialAnswerForm.style.display = 'block';
   });
-  
-  // ページが読み込まれたときに実行する関数を定義
+
   $(document).ready(function() {
-    // "modal-button"がクリックされたときの処理
     $('#modal-button').click(function() {
-      // ポップアップを表示
       $('#modal').show();
     });
 
-    // "disagree-button"がクリックされたときの処理
     $('#disagree-button').click(function() {
-      // ポップアップを非表示
       $('#modal').hide();
     });
   });
-
 });
