6369a3e62ad9243287c27c6e6b3fa68d9b0ca783
strohynis <85390184+strohynis@users.noreply.github.com>
2023-10-10 23:36:45 +0900

    報酬機能の処理部分を実装

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -33,11 +33,11 @@ class AnswersController < ApplicationController
     # ①"confirm-answer-btn"を押した日時のタイムスタンプを保存する。
     @post.update(confirmed_at: Time.now)
   
-    # ②Postsテーブルの"is_resolved"カラムに"1"の値を入れる。
-    @post.update(is_resolved: 1)
+    # ②Postsテーブルの"is_resolved"カラムに"true"の値を入れる。
+    @post.update(is_resolved: true)
   
-    # ③Answersテーブルの"is_selected_correct_answer"カラムに"1"の値を入れる。
-    @answer.update(is_selected_correct_answer: 1)
+    # ③Answersテーブルの"is_selected_correct_answer"カラムに"true"の値を入れる。
+    @answer.update(is_selected_correct_answer: true)
   
     # ④Postsテーブルの"resolved_user_id"カラムにUsersテーブルの対象のユーザー（選ばれたanswerをしたユーザー）のidを入れる。
     @post.update(resolved_user_id: @user.id)
@@ -45,8 +45,8 @@ class AnswersController < ApplicationController
     # ⑤"resolved_user_id"カラムに入ったidをもとに、Usersテーブルの対象のユーザーの"total_points"カラムに"10"の値を追加する。
     @user.increment!(:total_points, 10)
   
-    # ⑦Answersテーブルの"points_awarded"カラムに"1"の値を入れる。
-    @answer.update(points_awarded: 1)
+    # ⑦Answersテーブルの"points_awarded"カラムに"true"の値を入れる。
+    @answer.update(points_awarded: true)
   end 
 
   private
