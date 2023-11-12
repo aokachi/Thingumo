41ad5edccf867b91c6d9e24c0b36ee952b2b90fe
strohynis <85390184+strohynis@users.noreply.github.com>
2023-11-13 01:29:33 +0900

    イメージ手書き入力フォームが表示されるように修正

--- app/assets/javascripts/handwrite-image.js
+++ app/assets/javascripts/handwrite-image.js
@@ -1,35 +1,38 @@
-import { fabric } from 'fabric';
-window.fabric = fabric;
-
 window.onload = function() {
   var modal = document.getElementById('handwrite-image-modal');
   var btn = document.getElementById('handwrite-image-button');
   var span = document.getElementById('handwrite-image-modal-close');
+  var sendBtn = document.getElementById('handwrite-image-modal-send');
 
-  btn.onclick = function() {
+  // モーダルを表示するイベントリスナー
+  btn.addEventListener('click', function() {
     modal.style.display = "block";
-  }
+    canvas.setWidth(modal.clientWidth * 0.9);
+    canvas.setHeight(modal.clientHeight * 0.9);
+    canvas.calcOffset();
+  });
 
-  span.onclick = function() {
+  // モーダルを閉じるイベントリスナー
+  span.addEventListener('click', function() {
     modal.style.display = "none";
-  }
+  });
 
-  window.onclick = function(event) {
+  // モーダル外のクリックでモーダルを閉じるイベントリスナー
+  window.addEventListener('click', function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
-  }
+  });
 
+  // キャンバスの設定
   var canvas = new fabric.Canvas('handwrite-image-canvas');
   canvas.isDrawingMode = true;
   canvas.freeDrawingBrush.width = 5;
   canvas.freeDrawingBrush.color = "black";
 
-  btn.onclick = function() {
-    modal.style.display = "block";
-    canvas.setWidth(modal.clientWidth * 0.9);
-    canvas.setHeight(modal.clientHeight * 0.9);
-    canvas.calcOffset();
-  }
-
+  // 送信ボタンの機能（ここに必要なコードを追加）
+  sendBtn.addEventListener('click', function() {
+    // 送信処理の実装
+    // 例: canvas.toDataURL() を使用してキャンバスのデータを取得し、送信するなど
+  });
 }
