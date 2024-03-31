a2cf0221ed8a1bbaa956677d07d81d8572d92ef7
strohynis <85390184+strohynis@users.noreply.github.com>
2024-03-31 20:16:47 +0900

    回答の入力サジェスト機能の作成  - 不要になったコードの削除  - 各コードの説明の追加

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -7,13 +7,17 @@ class Answer < ApplicationRecord
   validate :does_not_contain_inappropriate_words
   validates :text, presence: true, length: { maximum: 30 }
 
+  validate :is_wikipedia_title
+  validate :does_not_contain_inappropriate_words
+  validates :text, presence: true, length: { maximum: 30 }
+
+  # WikidediaのAPIから検索サジェスト機能を呼び出す
   def is_wikipedia_title
     page = Wikipedia.find(text)
-    if page.text.nil?
-      errors.add(:text, "送信できません")
-    end
   end
 
+  # 回答に不適切な単語が含まれていないかチェックする
+  # inappropriate_words配列に不適切な単語をリストアップする
   def does_not_contain_inappropriate_words
     inappropriate_words = []
     if inappropriate_words.any? { |word| text.include?(word) }
--- spec/helpers/mecab_helper_spec.rb
+++ spec/helpers/mecab_helper_spec.rb
@@ -1,12 +1,13 @@
+# MeCabとNeologdを用いた形態素解析が正しく機能するか確認する
 require 'rails_helper'
 
 RSpec.describe 'MeCab and Neologd' do
   it '正しく形態素解析ができる' do
     begin
-      # MeCabのインスタンスを作成
+      # MeCabのライブラリが利用可能か確認する
       nm = Natto::MeCab.new
     rescue => e
-      Rails.logger.error "MeCabの初期化に失敗しましました。: #{e.message}"
+      Rails.logger.error "MeCabの初期化に失敗しました: #{e.message}"
       raise
     end
 
@@ -19,7 +20,7 @@ RSpec.describe 'MeCab and Neologd' do
       results.push(n.surface)
     end
 
-    # Neologdが正しく使われているかの確認（ここでは適当な単語で確認）
+    # Neologdが正しく使われているかの確認
     expect(results).to include("猫","ミーム")
   end
 end
