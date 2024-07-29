e4b5ccde1ecff81a6ad58efa6d6d27278d6cbed2
strohynis <85390184+strohynis@users.noreply.github.com>
2024-07-30 03:07:39 +0900

    fix: ユーザー情報表示ページの「編集」ボタンの配置と押下時の挙動を修正

--- app/assets/javascripts/handwrite-image.js
+++ app/assets/javascripts/handwrite-image.js
@@ -36,7 +36,7 @@ window.onload = function() {
     modal.style.display = "block";
 
     // 背後の要素のpointer-eventsをnoneに設定
-    var elementsToDisable = document.querySelectorAll('.form-control, .upload-zone, .new-question-select, .button-1, .button-2');
+    var elementsToDisable = document.querySelectorAll('.form-control, .upload-zone, .new-question-select, .btn3, .btn4');
     elementsToDisable.forEach(function(el) {
       el.style.pointerEvents = 'none';
     });
@@ -62,7 +62,7 @@ window.onload = function() {
     modal.style.display = "none";
 
     // 背後の要素のpointer-eventsを元に戻す
-    var elementsToDisable = document.querySelectorAll('.form-control, .upload-zone, .new-question-select, .button-1, .button-2');
+    var elementsToDisable = document.querySelectorAll('.form-control, .upload-zone, .new-question-select, .btn3, .btn4');
     elementsToDisable.forEach(function(el) {
       el.style.pointerEvents = 'auto';
     });
