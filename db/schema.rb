5bb5f18602473fe85a083bfbea59c98df465b70d
strohynis <85390184+strohynis@users.noreply.github.com>
2024-08-19 18:21:16 +0900

    feat: Active Storageを導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_09_102627) do
+ActiveRecord::Schema.define(version: 2024_08_19_091933) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
-    t.text "text"
-    t.bigint "user_id"
-    t.bigint "post_id"
-    t.boolean "is_selected_correct_answer", default: false
-    t.boolean "points_awarded"
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.bigint "question_id", null: false
+    t.boolean "is_selected_correct_answer", default: false, null: false
+    t.boolean "points_awarded", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "pending", default: false, null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
@@ -28,14 +50,14 @@ ActiveRecord::Schema.define(version: 2023_10_09_102627) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "post_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "question_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "post_id"
-    t.index ["post_id"], name: "index_post_images_on_post_id"
+    t.integer "question_id"
+    t.index ["question_id"], name: "index_question_images_on_question_id"
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.json "images"
@@ -50,19 +72,21 @@ ActiveRecord::Schema.define(version: 2023_10_09_102627) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.integer "sex"
-    t.date "birthday"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.integer "sex", null: false
+    t.date "birthday", null: false
     t.string "avatar"
     t.text "self_introduction"
-    t.bigint "total_points"
+    t.bigint "total_points", null: false
     t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
